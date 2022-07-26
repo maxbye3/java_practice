3f170c0cf4fdd313302956bf486d1f0848c78b83
Max Bye <mbye@techsoup.org>
2022-07-26 13:24:43 +0100

    are same kata

---
+++ src/AreSameKata.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class AreSameKata {
+    public static void main(String[] args) throws Exception {
+        // int[] a = {121, 144, 19, 161, 19, 144, 19, 11};
+        // int[] b = {121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        int[] a = {121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = {121, 14641, 20736, 36100, 25921, 361, 20736, 361};
+        System.out.print(comp(a, b));
+    }  
+    
+    public static boolean comp(final int[] a, final int[] b) {
+        return a != null && b != null && a.length == b.length && Arrays.equals(Arrays.stream(a).map(i -> i * i).sorted().toArray(), Arrays.stream(b).sorted().toArray());
+    }
+
+}
